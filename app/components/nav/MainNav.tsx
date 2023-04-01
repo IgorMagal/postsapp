330f3dcc04a0ee3c20a70a5dd6929252dbdc2577
Igor Magalhães <igortrapp30@gmail.com>
2023-04-01 14:22:17 -0400

    fiz?

--- app/components/nav/MainNav.tsx
+++ app/components/nav/MainNav.tsx
@@ -5,7 +5,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function MainNav() {
   const session = await getServerSession(authOptions);
-  console.log(session);
+  console.log(session?.user?.name);
 
   return (
     <nav className="fixed bg-opacity-50 py-2 pb-2 top-0 left-0 right-0 flex justify-between px-10 xs:px-4 items-center bg-neutral-800 border-b-neutral-300 border-b-2 z-10">
