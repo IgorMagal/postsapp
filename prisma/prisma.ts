2ac151abaab5ea5cb04d9fcac26b5204673e6832
IgorMagal <igortrapp30@gmail.com>
2023-04-09 00:41:05 -0400

    failed attempt from mac

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,6 +1,15 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+let prisma: PrismaClient;
 
-export default client;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
