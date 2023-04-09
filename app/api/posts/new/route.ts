055763f2deb3381c6803a0946ed521b5057e7a0a
IgorMagal <igortrapp30@gmail.com>
2023-04-09 00:29:57 -0400

    fixing paths

--- app/api/posts/new/route.ts
+++ app/api/posts/new/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import prisma from "@/prisma/prisma";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export async function POST(request: Request, response: Response) {
   const session = await getServerSession(authOptions);
