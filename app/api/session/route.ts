3035e14bdc68de43cc4117264b17c8922e17eb53
Igor Magalhães <igortrapp30@gmail.com>
2023-04-09 22:01:55 -0400

    more fixes tryingo to make auth work

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,5 +1,6 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
